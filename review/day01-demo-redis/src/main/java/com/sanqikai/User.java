f2d67fa350882bbf7cdc29704655ec53365b996c
zhangxiaosheng <15222322879@163.com>
2020-10-22 11:26:13 +0800

    第一版本

---
+++ review/day01-demo-redis/src/main/java/com/sanqikai/User.java
@@ -0,0 +1,34 @@
+package com.sanqikai;
+
+/**
+ * @author zhangxiaosheng
+ * @date 2020/10/22 - 11:21
+ */
+public class User {
+    private String username;
+    private Integer age;
+
+    public User(String username, Integer age) {
+        this.username = username;
+        this.age = age;
+    }
+
+    public User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
